4186e00bae1eb17a96d24ba8e8bba0e9f9c3a002
Dmitriy Artemenko <dimchik.xai@gmail.com>
2016-01-20 15:44:24 +0200

    Sample code to detect adblock and sandbox mode.

---
+++ js/app.js
@@ -0,0 +1,52 @@
+var Application = {
+
+    init: function () {
+        var self = this;
+
+        if (typeof fuckAdBlock === 'undefined') {
+            this._adBlockDetected();
+        } else {
+            fuckAdBlock.on(true, self._adBlockDetected).onNotDetected(self._adBlockNotDetected);
+        }
+
+        var sandboxCheckResult = sandblaster.detect();
+
+        if (sandboxCheckResult.sandboxed) {
+            this._sandboxDetected();
+        } else {
+            this._sandboxNotDetected();
+        }
+    },
+
+    _adBlockDetected: function () {
+        var adBlockStatusContainer = document.getElementById('adBlockStatus');
+        adBlockStatusContainer.textContent = 'AdBlock is enabled';
+        adBlockStatusContainer.className += ' detected';
+
+        console.log('AdBlock is enabled');
+    },
+
+    _adBlockNotDetected: function () {
+        var adBlockStatusContainer = document.getElementById('adBlockStatus');
+        adBlockStatusContainer.textContent = 'AdBlock is not enabled';
+        adBlockStatusContainer.className += ' not-detected';
+
+        console.log('AdBlock is not enabled');
+    },
+
+    _sandboxDetected: function () {
+        var sandboxStatusContainer = document.getElementById('sandboxStatus');
+        sandboxStatusContainer.textContent = 'Sandbox is enabled';
+        sandboxStatusContainer.className += ' detected';
+
+        console.log('Sandbox is enabled');
+    },
+
+    _sandboxNotDetected: function () {
+        var sandboxStatusContainer = document.getElementById('sandboxStatus');
+        sandboxStatusContainer.textContent = 'Sandbox is not enabled';
+        sandboxStatusContainer.className += ' not-detected';
+
+        console.log('Sandbox is not enabled');
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+    Application.init();
+};
